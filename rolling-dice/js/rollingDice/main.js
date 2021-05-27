8937633369544c1bd592e1b4252609c2654ca4c6
srcrank <salmacrank@gmail.com>
2021-05-27 15:25:55 -0500

    rolling the dice!

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------")
+
+const diceRoll = () => {
+
+for( let i = 0; i <= 10; i++) {
+    let diceOne = Math.floor(Math.random() * 6) + 1
+    let diceTwo = Math.floor(Math.random() * 6) + 1
+    if (diceOne === diceTwo) {
+        console.log(`${diceOne} + ${diceTwo} == ${diceOne + diceTwo} DOUBLES!`)
+    }
+    else {
+        console.log(`${diceOne} + ${diceTwo} == ${diceOne + diceTwo}`)
+    }
+}}
+
+diceRoll();
