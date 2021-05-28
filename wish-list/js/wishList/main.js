9a71d607f3505757e2da0c8582111f98fac8bbc7
srcrank <salmacrank@gmail.com>
2021-05-28 02:19:06 -0500

    translate 2

--- wish-list/js/wishList/main.js
+++ wish-list/js/wishList/main.js
@@ -5,7 +5,7 @@ function main() {
     "A washtub of Skittles",
     "World peace, but not the kind of world peace where all the humans are removed from existence",
     "A TV that is NOT internet-capable",
-    "The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"
+    "The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?",
   ];
 
   console.log("My wishes...");
